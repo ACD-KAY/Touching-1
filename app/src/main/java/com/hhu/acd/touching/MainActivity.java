93e15fc6c256b7121cfcf14a60dbb1d91354f2e9
TeamACD <hhuacd2018>
2018-01-17 18:48:26 +0800

    Initial commit

---
+++ app/src/main/java/com/hhu/acd/touching/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hhu.acd.touching;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/hhu/acd/touching/mainactivity_3_messagelist.java
@@ -0,0 +1,43 @@
+package com.hhu.acd.touching;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
+/**
+ * Created by liziming on 18-1-17.
+ */
+
+public class mainactivity_3_messagelist extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity_3_messagelist);
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+        bottomNavigationBar
+                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC
+                );
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_people_outline_black_48dp, "会议").setActiveColorResource(R.color.primary_light))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_message_black_48dp, "消息").setActiveColorResource(R.color.primary_light))
+                .setFirstSelectedPosition(0)
+                .initialise();
+
+        //bottomNavigationBar.setTabSelectedListener(this);
+        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener(){
+            @Override
+            public void onTabSelected(int position) {
+            }
+            @Override
+            public void onTabUnselected(int position) {]
+            }
+            @Override
+            public void onTabReselected(int position) {
+            }
+        });
+    }
+
+
+}
