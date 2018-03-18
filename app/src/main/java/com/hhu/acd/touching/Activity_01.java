ec7a3b200f1857936ce684109a4a84b5cb47c22f
TeamACD <woshiliziming@gmail.com>
2018-03-18 14:42:12 +0800

    318

--- app/src/main/java/com/hhu/acd/touching/Activity_01.java
+++ app/src/main/java/com/hhu/acd/touching/Activity_01.java
@@ -7,21 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Activity_01 extends AppCompatActivity {
     private Button btn;
     private TextView btn2;
+    private EditText account;
+    private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_01);
-
+        account=findViewById(R.id.account);
+        password=findViewById(R.id.password);
         /** when the user clicks "登录"*/
         btn=(Button)findViewById(R.id.B_login);
         btn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                String id=account.getText().toString();
+                String pw=password.getText().toString();
                 Intent it = new Intent(Activity_01.this, Activity_03.class);
                 startActivity(it);
             }
---
+++ app/src/main/java/com/hhu/acd/touching/NimApplication.java
@@ -0,0 +1,71 @@
+package com.hhu.acd.touching;
+
+import android.app.Application;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Environment;
+import android.text.TextUtils;
+
+import com.netease.nimlib.sdk.NIMClient;
+import com.netease.nimlib.sdk.SDKOptions;
+import com.netease.nimlib.sdk.StatusBarNotificationConfig;
+import com.netease.nimlib.sdk.auth.LoginInfo;
+import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum;
+import com.netease.nimlib.sdk.uinfo.UserInfoProvider;
+import com.netease.nimlib.sdk.uinfo.model.UserInfo;
+import com.netease.nimlib.sdk.util.NIMUtil;
+
+import java.io.IOException;
+
+
+/**
+ * Created by liziming on 18-3-18.
+ */
+
+public class NimApplication extends Application {
+    @Override
+    public void onCreate() {
+        // ... your codes
+        super.onCreate();
+        // SDK初始化（启动后台服务，若已经存在用户登录信息， SDK 将完成自动登录）
+        NIMClient.init(this, loginInfo(), options());
+
+        // ... your codes
+        if (NIMUtil.isMainProcess(this)) {
+            // 注意：以下操作必须在主进程中进行
+            // 1、UI相关初始化操作
+            // 2、相关Service调用
+        }
+    }
+
+    // 如果返回值为 null，则全部使用默认参数。
+    private SDKOptions options() {
+        return null;
+    }
+
+    // 如果已经存在用户登录信息，返回LoginInfo，否则返回null即可
+    private LoginInfo loginInfo() {
+        return null;
+    }
+
+    /**
+     * 配置 APP 保存图片/语音/文件/log等数据的目录
+     * 这里示例用SD卡的应用扩展存储目录
+     */
+    static String getAppCacheDir(Context context) {
+        String storageRootPath = null;
+        try {
+            // SD卡应用扩展存储区(APP卸载后，该目录下被清除，用户也可以在设置界面中手动清除)，请根据APP对数据缓存的重要性及生命周期来决定是否采用此缓存目录.
+            // 该存储区在API 19以上不需要写权限，即可配置 <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" android:maxSdkVersion="18"/>
+            if (context.getExternalCacheDir() != null) {
+                storageRootPath = context.getExternalCacheDir().getCanonicalPath();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return storageRootPath;
+    }
+}
