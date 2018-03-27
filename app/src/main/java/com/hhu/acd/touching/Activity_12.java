7d633b1069309a0869b7096e30e2d3ea7ecc4528
TeamACD <35477222+TeamACD@users.noreply.github.com>
2018-03-27 10:21:59 +0800

    Update Activity_12.java

--- app/src/main/java/com/hhu/acd/touching/Activity_12.java
+++ app/src/main/java/com/hhu/acd/touching/Activity_12.java
@@ -7,12 +7,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
+import android.view.View;
+import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+
 public class Activity_12 extends AppCompatActivity {
+    
+     private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_12);
+        
+         /**标题栏设置*/
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("");
+
+
+        /** 返回到前一个界面"*/
+        btn=(Button)findViewById(R.id.B_before);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        
+        
     }
 }
